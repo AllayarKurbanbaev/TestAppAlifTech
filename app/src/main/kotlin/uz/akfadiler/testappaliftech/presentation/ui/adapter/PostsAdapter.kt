98c2b273ba5942097897b264e58d1d2a05051210
Allayar Kurbanbaev <allayar.kurbanbaev99@gmail.com>
2023-01-03 20:44:01 +0500

    03.01.2023 20:43

---
+++ app/src/main/kotlin/uz/akfadiler/testappaliftech/presentation/ui/adapter/PostsAdapter.kt
@@ -0,0 +1,42 @@
+package uz.akfadiler.testappaliftech.presentation.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import uz.akfadiler.testappaliftech.data.remote.response.posts.PostResponse
+import uz.akfadiler.testappaliftech.databinding.ItemPostsBinding
+
+class PostsAdapter : ListAdapter<PostResponse, PostsAdapter.PostsViewHolder>(PostsDiffUtils) {
+
+    inner class PostsViewHolder(private val binding: ItemPostsBinding) : ViewHolder(binding.root) {
+
+        fun bind() {
+
+        }
+    }
+
+    private object PostsDiffUtils : DiffUtil.ItemCallback<PostResponse>() {
+        override fun areItemsTheSame(oldItem: PostResponse, newItem: PostResponse): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: PostResponse, newItem: PostResponse): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsViewHolder {
+        return PostsViewHolder(
+            ItemPostsBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: PostsViewHolder, position: Int) {
+        holder.bind()
+    }
+}
